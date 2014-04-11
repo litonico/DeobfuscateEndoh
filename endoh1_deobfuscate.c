af66f00c0e334ffce1b37ca07acc04c8eed6c25b
LitoNico <lito.nicolai@gmail.com>
2014-04-11 00:33:39 -0700

    Extra descriptions of for-loop assignment tricks

--- endoh1_deobfuscate.c
+++ endoh1_deobfuscate.c
@@ -2,15 +2,26 @@
 // G -> Gravity
 // P -> Pressure
 // V -> Velocity
+// these are read in from command line args
 
-// Essential info: in C, a = b EVALUATES to 0, and performs an assignment as a 'side effect'
+// Endoh:
+// Though some assignments may look meaningless, it is actually meaningful; 
+// it extracts “integer part of real part” from a complex value by assigning 
+// (and casting) it to an integer-type variable.
+
+// Essential info: in C, a = b EVALUATES to 0, and performs an assignment 
+// as a 'side effect.' Assignment in the initialization of a for loop will 
+// make the compiler complain; when he uses this, Mr. Endoh wraps the 
+// expressions in parens to silence that.
+//
+
 #include <stdio.h>//  .IOCCC                                         Fluid-  #
 #include <unistd.h>  //2012                                         _Sim!_  #
 #include <complex.h>  //||||                     ,____.              IOCCC-  #
+
+double complex	a[97687],
 // Endoh: "Every five entries of double complex a[] contain information
 // of one particle: position, wall-flag, density, force, and velocity.
-//
-double complex	a[97687],
        *p, *q, *r = a, w = 0, d;
 int	x, y;
 char b[6856] = "\x1b[2J" //clears the screen (ANSI escape code). '\x1b' is equivalent to ESC.
@@ -26,7 +37,7 @@ int main(){
 
 	for (;; puts(o), o = b + 4) { 
         // while(true){ 
-        // puts(o)
+        // puts(o) // clears the screen
         // o = b + 4 // o points to the 4th elem of b
 		for (p = a; p[2] = p[1] * 9, p < r; p += 5)
 			for (q = a; w = cabs(d = *p - *q) / 2 - 1, q < r; q += 5)
