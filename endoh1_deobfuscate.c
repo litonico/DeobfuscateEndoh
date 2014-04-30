55ddc4fe88b8b8142dba809398b0377009675afe
LitoNico <lito.nicolai@gmail.com>
2014-04-30 01:44:12 -0700

    Cleared up _Complex_I

--- endoh1_deobfuscate.c
+++ endoh1_deobfuscate.c
@@ -44,8 +44,9 @@ char *o = b, *t;
 int main(){
 
 	for (; 0 < (x = getc(stdin));){
-		w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - I : (x = w + 2); // why the heck were there two semicolons
+        w = x > 10 ? 32 < x ? 4[*r++ = w, r] = w + 1, *r = r[5] = x == 35, r += 9 : 0, w - _Complex_I : (x = w + 2); // why the heck were there two semicolons    
     }
+ 
 
 	for (;; puts(o), o = b + 4) { 
         // while(true){ 
@@ -72,7 +73,7 @@ int main(){
 			b[x] = 0;
         }
 
-		for (p = a; (t = b + 10 + (x = *p * I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5) {
+		for (p = a; (t = b + 10 + (x = *p * _Complex_I) + 80 * (y = *p / 2), *p += p[4] += p[3] / 10 * !p[1]), p < r; p += 5) {
 			x = 0 <= x && x < 79 && 0 <= y && y < 23 ? 1[1[*t |= 8, t] |= 4, t += 80] = 1, *t |= 2 : 0;
         }
 
